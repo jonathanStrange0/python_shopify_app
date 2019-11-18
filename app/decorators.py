86e4f4e0950e3f2f10dec80a5b6d7c6f202e40a6
Jonathan Mucha <jonmucha@gmail.com>
2019-11-18 05:59:16 -0500

    planning out the workflow of install/auth of app

---
+++ app/decorators.py
@@ -0,0 +1,3 @@
+from functools import wraps
+import shopify
+from flask import session, redirect, url_for, request, current_app
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
 from app import app
+import shopify
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return 'Hello World'
+
+
+@app.route('/shopify'):
+def shopify():
+    #TODO: When directed to this route, user should be prompted to
+    # install the app on their store, or if already installed
+    # they should be redirected to the callback function
+    pass
+
+@app.route('/shopify/callback')
+def callback():
+    # TODO: After install/auth return the homepage of the app
+    # that will be embedded in the user's store.
+    pass
