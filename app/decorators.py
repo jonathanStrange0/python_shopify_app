baa8408833ec5870043920eb0af80f0d72eef680
Jonathan Mucha <jonmucha@gmail.com>
2019-11-20 05:43:47 -0500

    managed to get redirected to the shopify store on login, not sure why

--- app/decorators.py
+++ app/decorators.py
@@ -1,9 +1,34 @@
 from functools import wraps
-import shopify
+import shopify, os
 from flask import session, redirect, url_for, request, current_app
+from dotenv import load_dotenv
+
+
 
 def shopify_auth_required(function):
     @wraps(function)
     def decorated_function(*args, **kwargs):
+        load_dotenv()
         #TODO add authorization code here
-        pass
+        if 'shopify_token' not in session:
+            shop_url = request.args.get('shop')
+            shopify.Session.setup(
+                api_key=os.getenv('SHOPIFY_API_KEY'),
+                secret=os.getenv('SHOPIFY_SHARED_SECRET'))
+            try:
+                shop_session = shopify.Session.validate_params(request.args)
+                print('Parameters verified in decoration function. \n session: ', shop_session)
+            except Exception as ex:
+                return redirect(url_for('app.shopify', **request.args))
+        else:
+            try:
+                session.pop("shopify_token")
+                session.pop("shopify_url")
+                session.pop("shopify_id")
+                return redirect(url_for('shopify_bp.install', **request.args))
+            except Exception as e:
+                return('There has been an error: ', e)
+
+        return function(*args, **kwargs)
+
+    return decorated_function
--- app/routes.py
+++ app/routes.py
@@ -1,44 +1,53 @@
 from app import app
-import shopify
+import shopify as sfy
 from flask import session, redirect, url_for, request, current_app
+from app.decorators import shopify_auth_required
+from dotenv import load_dotenv
+import os
 
 
 @app.route('/')
 @app.route('/index')
+# @shopify_auth_required
 def index():
     return 'Hello World'
 
-
+@shopify_auth_required
 @app.route('/shopify')
 def shopify():
+    load_dotenv()
     #TODO: When directed to this route, user should be prompted to
     # install the app on their store, or if already installed
     # they should be redirected to the callback function
 
-    #get the shop name from the request kwargs
+    #get the shop name from the request args
     shop_url = request.args.get('shop')
+    print('shop_url: ', shop_url)
     if shop_url:
         # setup a shopify session
-        shopify.Session.setup(
-            api_key=current_app.config['SHOPIFY_API_KEY'],
-            secret=['SHOPIFY_SHARED_SECRET']
+        sfy.Session.setup(
+            api_key=os.getenv('SHOPIFY_API_KEY'),
+            secret=os.getenv('SHOPIFY_SHARED_SECRET')
         )
-
         #Create a shopify session instance with api version and url for shop
-        shop_session = shopify.Session(shop_url, '2019-04')
+        shop_session = sfy.Session(shop_url, '2019-04')
+        print('shopify session: ', shop_session)
 
         #Define the access scopes the app would like to have
         scope = ["write_products", "read_products"] #may add more later
 
         #Generate the permissions url:
-        permission_url = shop_session.create_permission_url(scope)
+        permission_url = shop_session.create_permission_url(scope, url_for('callback', _external=True))
+        print('Permission URL: ', permission_url)
+        #TODO: Fix this redirect, it don't work yo.
         return(redirect(permission_url))
     else:
         return 'Missing shop parameter. Please add ?shop=your-development-shop.myshopify.com to your request'
-    
+
 
 @app.route('/shopify/callback')
 def callback():
     # TODO: After install/auth return the homepage of the app
     # that will be embedded in the user's store.
-    pass
+    return('Successfully redirected')
+    # pass
