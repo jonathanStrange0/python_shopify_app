fa71ae434baa33c24387b4f3367b9a9f55fb1b0b
Jonathan Mucha <jonmucha@gmail.com>
2019-12-23 10:40:41 -0500

    built database

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from app import db
+
+class Product(db.Model):
+    gid = db.Column(db.String(64), primary_key=True)
+    name = db.Column(db.Strinig(128), index=True)
+    variants = db.relationship('Variant', backref='variant',\
+                            cascade='save-update, delete, delete-orphan')
+
+class Variant(db.Model):
+    gid = db.Column(db.String(64), primary_key=True)
+    name = db.Column(db.Strinig(128), index=True)
+    product_gid = db.Column(db.String(64), db.ForeignKey('product.gid'))
+
+
+class Customer(db.Model):
+    gid = db.Column(db.String(64), primary_key=True)
+    firast_name = db.Column( db.String(64), index=True)
+    last_name = db.Column( db.String(64), index=True)
